96d746889d0baf4197d2c5f64fb1c1d01ae3fda5
TriTamLe <tritam.le54@gmail.com>
2023-12-15 21:02:12 +0700

    add index.js file and add empty get method

--- src/routes/rsa.route.js
+++ src/routes/rsa.route.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router()
 const rsaController = require('../app/controllers/rsa.controller')
 
+router.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Hello World',
+  })
+})
 router.post('/key-generate', rsaController.generateKey)
 router.post('/encrypt', rsaController.encrypt)
 router.post('/decrypt', rsaController.decrypt)
